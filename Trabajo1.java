d4183e880c03bc622e7fa9c771da057a5574dbb7
Xbladexx <martinezmunozam@gmail.com>
2024-08-06 17:47:26 -0500

    Updated

---
+++ Trabajo1.java
@@ -0,0 +1,10 @@
+
+public class Trabajo1 {
+
+     public static void main(String[] args) {
+        System.out.println( "Hola Mundo");
+        System.out.println("probando git hub xdd , ola jeremy del futuro");
+     }
+}
+
+
